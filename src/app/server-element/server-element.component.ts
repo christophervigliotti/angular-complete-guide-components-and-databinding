dfb3c1c21c60408d05458ae4acdb9eb6d76b3f2b
Christopher Vigliotti <christopher@christophervigliotti.com>
2022-02-01 07:35:22 -0500

    79 broken!

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, ViewEncapsulation, OnInit } from '@angular/core';
+import { 
+  Component, 
+  Input, 
+  ViewEncapsulation, 
+  OnInit, 
+  OnChanges, 
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -12,4 +19,18 @@ export class ServerElementComponent implements OnInit {
     name: string,
     content: string
   };
+  @Input() name: string;
+
+  constructor(){
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
 }
